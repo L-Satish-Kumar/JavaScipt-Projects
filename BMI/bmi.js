eeb1a24ae88dc3f2970f8e79b5d612f091ba9314
L-Satishkumar <satishkumar.l@promantia.com>
2026-02-22 17:20:57 +0530

    Body Mass Index.

---
+++ BMI/bmi.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let height = parseInt(document.querySelector('#height').value);
+    let weight = parseInt(document.querySelector('#weight').value);
+    let result = document.querySelector('#results');
+
+    if (height === "" || height < 0 || isNaN(height) ){
+        result.innerHTML = 'Please enter a valid height';
+    } else if (weight === "" || weight < 0 || isNaN(weight) ){
+        result.innerHTML = 'Please enter a valid weight';
+    } else {
+        let bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        result.innerHTML = `<span>${bmi}</span>`;
+    
+    }
+
+});
